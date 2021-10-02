a3591bec943b04577a51c5959e472726ac4871de
nil1729 <nilanjan172nsvian@gmail.com>
2021-10-02 12:45:53 +0530

    test yml

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 # Decrypt the file
-echo $HOME
-pwd
-ls -al
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/runner/work/food-order-app/food-order-app/config/secret.json admin.json.gpg
+--output ./config/secret.json admin.json.gpg
 
-# gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
-# --output $HOME/client/src/firebase/config.js firebase-config.js.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output ./client/src/firebase/config.js firebase-config.js.gpg
