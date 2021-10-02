ce5a6d4c65425059eb9554bef125155d0826c3f0
nil1729 <nilanjan172nsvian@gmail.com>
2021-10-02 12:43:28 +0530

    test yml

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # Decrypt the file
+echo $HOME
+pwd
+ls -al
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
