3936d4962688019f111b879c8e25b83c4ffbb78d
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2022-04-15 03:55:41 +0530

    update order count and env setup

--- routes/admin.js
+++ routes/admin.js
@@ -8,34 +8,32 @@ const User = require('../models/User');
 const Order = require('../models/Order');
 
 router.get('/admin/stats', verifyAuth, async (req, res) => {
-    try {
-        if (!req.siteAdmin) {
-            return res.status(403).json({
-                msg: 'Unauthorized Access',
-            });
-        }
+	try {
+		if (!req.siteAdmin) {
+			return res.status(403).json({
+				msg: 'Unauthorized Access',
+			});
+		}
 
-        const orderCount = await Order.countDocuments();
-        const productCount = await Product.countDocuments();
-        const customerCount = await User.countDocuments({
-            isAdmin: false
-        });
+		const orderCount = await Order.countDocuments({ isPurchased: true });
+		const productCount = await Product.countDocuments();
+		const customerCount = await User.countDocuments({
+			isAdmin: false,
+		});
 
-        return res.status(200).json({
-            stats: {
-                orderCount,
-                productCount,
-                customerCount,
-            }
-        });
-
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({
-            msg: 'Oops! Server error occurred'
-        });
-    }
+		return res.status(200).json({
+			stats: {
+				orderCount,
+				productCount,
+				customerCount,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({
+			msg: 'Oops! Server error occurred',
+		});
+	}
 });
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
-}
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const auths = require('./routes/auths');
